a5f444c0bdab10436965622ec80fc4c33801ceb1
entinfx <monkacry@gmail.com>
2022-10-08 16:23:17 +0500

    Working app.

---
+++ auth_server.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+const refreshTokens = [] // normally stored in DB or Redis Cache
+
+app.listen(4000, () => {
+    console.log('Listening on 3000')
+})
+
+app.use(express.json())
+
+app.post('/login', (req, res) => {
+    // * Authenticate user (find requested user in DB) [CODE OMITTED]
+    const username = req.body.username
+    const user = { name: username } // faking db authentication code
+
+    // * If user found - issue access and refresh tokens
+    const accessToken = generateAccessToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken) // store refresh tokens for safety
+    res.json({ accessToken, refreshToken })
+})
+
+app.post('/refresh_token', (req, res) => {
+    const refreshToken = req.body.refresh_token
+    if (!refreshToken) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (error, user) => {
+        if (error) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name })
+        res.json({ accessToken })
+    })
+})
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m' })
+}
--- server.js
+++ server.js
@@ -1,6 +1,32 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
 
 app.listen(3000, () => {
     console.log('Listening on 3000')
 })
+
+app.use(express.json())
+
+const posts = [
+    { username: "Jim", title: "I like Pam!"},
+    { username: "Dwight", title: "MICHAEL!"}
+]
+
+app.get('/posts', authenticateAccessToken, (req, res) => {
+    res.json(posts.filter(p => p.username === req.user.name))
+})
+
+function authenticateAccessToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader?.split(' ')[1]
+    if (!token) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+        if (error) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
